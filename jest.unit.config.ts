29d50da264a4875c3139fb81355dce050fc45fd2
Ulad Kasach <uladkasach@gmail.com>
2026-01-11 17:13:49 -0600

    fix(cicd): unblock release

--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -14,16 +14,18 @@ const config: Config = {
   verbose: true,
   reporters: [['default', { summaryThreshold: 0 }]], // ensure we always get a failure summary at the bottom, to avoid the hunt
   testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'ts'],
+  moduleFileExtensions: ['js', 'ts', 'mjs'],
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.mjs$': '@swc/jest',
   },
   transformIgnorePatterns: [
-    // here's an example of how to ignore esm module transformation, when needed
-    // 'node_modules/(?!(@octokit|universal-user-agent|before-after-hook)/)',
+    // transform esm modules that jest would otherwise choke on
+    // pattern handles both direct node_modules and pnpm's .pnpm structure
+    '/node_modules/(?!(\\.pnpm/(@anthropic-ai))|(@anthropic-ai)/)',
   ],
   testMatch: [
     // note: order matters
